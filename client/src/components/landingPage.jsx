900af4082249f404638b5129cd5556618a730e85
Nicolas Larrosa <enzolarrosa@gmail.com>
2023-04-13 17:38:26 -0300

    arreglando cosas

--- client/src/components/landingPage.jsx
+++ client/src/components/landingPage.jsx
@@ -6,8 +6,8 @@ export default function Landing() {
     return (
         <Link to='/home'>
         <div className={s.backG}>
-            <h1 className={s.tit}>Clickee en cualquier parte chef </h1>
-            <h1 className={s.titl}>Clickee en cualquier parte chef </h1>
+            <h1 className={s.tit}>Clickeé en cualquier parte chef </h1>
+            <h1 className={s.titl}>Clickeé en cualquier parte chef </h1>
         </div>
         </Link>
     )
