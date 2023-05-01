3a6f70ca304ca9e61530323ce7ec64a91c0a6072
Nicolas Larrosa <enzolarrosa@gmail.com>
2023-05-01 06:10:56 -0300

    deploy finish

--- api/src/db.js
+++ api/src/db.js
@@ -7,29 +7,29 @@ const {
 ,DB_DEPLOY} = process.env;
 
 let sequelize=
-  process.env.NODE_ENV === 'production'
-   ? new Sequelize({
-    database: DB_NAME,
-    dialect: 'postgres',
-    host: DB_HOST,
-    port: 5432,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    pool: {
-      max:3,
-      min: 1,
-      idle: 10000,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
-    },
-    ssl: true,
-   })
-   : 
+  // process.env.NODE_ENV === 'production'
+  //  ? new Sequelize({
+  //   database: DB_NAME,
+  //   dialect: 'postgres',
+  //   host: DB_HOST,
+  //   port: 5432,
+  //   username: DB_USER,
+  //   password: DB_PASSWORD,
+  //   pool: {
+  //     max:3,
+  //     min: 1,
+  //     idle: 10000,
+  //   },
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false,
+  //     },
+  //     keepAlive: true,
+  //   },
+  //   ssl: true,
+  //  })
+  //  : 
   new Sequelize(DB_DEPLOY, {
     logging: false,
     native: false,
